46d72c7c2afa8ef896bcc350ea12fc6e0d410baf
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-09-30 02:58:20 -0300

    feat: ApplicationUser class now has required informations to db

--- UCLoan/Models/ApplicationUser.cs
+++ UCLoan/Models/ApplicationUser.cs
@@ -1,6 +1,19 @@
-﻿namespace UCLoan.Models
+﻿using System.ComponentModel.DataAnnotations;
+using Microsoft.AspNetCore.Identity;
+
+namespace UCLoan.Models
 {
-    public class ApplicationUser
+    public class ApplicationUser : IdentityUser
     {
+        [Required]
+        [Display(Name = "Nome completo: ")]
+        public string FullName { get; set; } = string.Empty;
+
+        [Required]
+        [Display(Name = "CPF:")]
+        public string CPF { get; set; } = string.Empty;
+
+        public ICollection<EquipmentHistory> ChangedHistories { get; set; } = new List<EquipmentHistory>();
+        public ICollection<EquipmentHistory> LastUserHistories { get; set; } = new List<EquipmentHistory>();
     }
 }
