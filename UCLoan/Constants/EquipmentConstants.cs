7c72d03692e0c4fbebef98f7fab13cf8d8ab214a
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-09-30 02:57:10 -0300

    feat: implement equipment and message constants for code pattern

--- UCLoan/Constants/EquipmentConstants.cs
+++ UCLoan/Constants/EquipmentConstants.cs
@@ -1,6 +1,43 @@
-﻿namespace UCLoan.Constants
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UCLoan.Constants
 {
     public class EquipmentConstants
     {
+        public enum EquipmentLoanStatus
+        {
+            [Display(Name = "Indisponível")]
+            Unavailable,
+
+            [Display(Name = "Disponível")]
+            Available,
+
+            [Display(Name = "Emprestado")]
+            Borrowed,
+
+            [Display(Name = "Em atraso")]
+            Overdue,
+
+            [Display(Name = "Devolvido")]
+            Returned,
+        }
+
+        public enum EquipmentPhysicalStatus
+        {
+            [Display(Name = "Excelente")]
+            Excellent,
+
+            [Display(Name = "Bom")]
+            Good,
+
+            [Display(Name = "Regular")]
+            Fair,
+
+            [Display(Name = "Ruim")]
+            Poor,
+
+            [Display(Name = "Quebrado")]
+            Broken,
+        }
     }
 }
--- UCLoan/Constants/MessageConstants.cs
+++ UCLoan/Constants/MessageConstants.cs
@@ -2,5 +2,7 @@
 {
     public class MessageConstants
     {
+        public static string SuccessMessage = "successMessage";
+        public static string ErrorMessage = "errorMessage";
     }
 }
