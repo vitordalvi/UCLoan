e28ed1fa9f3ab74f779d87777e6d24fc3e5824b4
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-09-30 03:09:51 -0300

    feat: add application default seeder, datasets to db

--- UCLoan/Data/ApplicationDbContext.cs
+++ UCLoan/Data/ApplicationDbContext.cs
@@ -1,13 +1,40 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using System.Reflection.Emit;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using UCLoan.Models;
 
 namespace UCLoan.Data
 {
-    public class ApplicationDbContext : IdentityDbContext
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser, IdentityRole, string>
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
         }
+
+        public DbSet<Loan> Loans { get; set; }
+        public DbSet<LoanHistory> LoanHistories { get; set; }
+        public DbSet<Equipment> Equipments { get; set; }
+        public DbSet<EquipmentModel> EquipmentModels { get; set; }
+        public DbSet<EquipmentHistory> EquipmentHistories { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<EquipmentHistory>()
+                .HasOne(eh => eh.ChangedBy)
+                .WithMany(u => u.ChangedHistories)
+                .HasForeignKey(eh => eh.ChangedById)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<EquipmentHistory>()
+                .HasOne(eh => eh.LastUser)
+                .WithMany(u => u.LastUserHistories)
+                .HasForeignKey(eh => eh.LastUserId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
     }
 }
--- UCLoan/Data/DbSeeder.cs
+++ UCLoan/Data/DbSeeder.cs
@@ -1,1 +1,57 @@
-﻿
+﻿using Microsoft.AspNetCore.Identity;
+
+using UCLoan.Models;
+
+namespace UCLoan.Data
+{
+    public static class DbSeeder
+    {
+        public static async Task SeedDatabaseAsync(IServiceProvider serviceProvider)
+        {
+            var roleManager = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+            var userManager = serviceProvider.GetRequiredService<UserManager<ApplicationUser>>();
+
+
+            // Criando cargos
+            var roles = new[] { "Admin", "User" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                {
+                    await roleManager.CreateAsync(new IdentityRole(role));
+                }
+            }
+
+            var adminEmail = "admin@gmail.com";
+            var adminPassword = "Admin@123";
+
+            var userExist = await userManager.FindByEmailAsync(adminEmail);
+
+            if (userExist == null)
+            {
+                var adminUser = new ApplicationUser
+                {
+                    UserName = "admin",
+                    FullName = "Admin",
+                    Email = adminEmail,
+                    EmailConfirmed = true
+                };
+
+                var result = await userManager.CreateAsync(adminUser, adminPassword);
+
+                if (result.Succeeded)
+                {
+                    await userManager.AddToRoleAsync(adminUser, "Admin");
+                }
+                else
+                {
+                    throw new Exception("Failed to create the admin user: " + string.Join(", ", result.Errors));
+                }
+            }
+
+
+
+        }
+    }
+}
