393fe00a70273c4431adf7e8c129d823421b160d
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-09-30 03:00:46 -0300

    feat: created loan modes information

--- UCLoan/Models/Loan.cs
+++ UCLoan/Models/Loan.cs
@@ -1,6 +1,17 @@
-﻿namespace UCLoan.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace UCLoan.Models
 {
+    [Table("Loans")]
     public class Loan
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public ApplicationUser User { get; set; } = null!;
+        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime? EndDate { get; set; }
     }
 }
