964d86330ad7d8f9d3efd148be5b17ab87c1498a
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-10-02 15:58:35 -0300

    fix: removed privacy actionresult

--- UCLoan/Controllers/HomeController.cs
+++ UCLoan/Controllers/HomeController.cs
@@ -1,5 +1,7 @@
 using System.Diagnostics;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using UCLoan.Models;
 
@@ -16,11 +18,6 @@ namespace UCLoan.Controllers
         }
 
         public IActionResult Index()
-        {
-            return View();
-        }
-
-        public IActionResult Privacy()
         {
             return View();
         }
