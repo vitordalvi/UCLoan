a0654591fb20c1f3a8d8ceb64fb1512160dac49f
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-10-02 14:56:38 -0300

    feat: login verification in home page

--- UCLoan/Controllers/HomeController.cs
+++ UCLoan/Controllers/HomeController.cs
@@ -1,9 +1,11 @@
 using System.Diagnostics;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using UCLoan.Models;
 
 namespace UCLoan.Controllers
 {
+    [Authorize]
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
