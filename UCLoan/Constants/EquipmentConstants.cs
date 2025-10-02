4c5fba64b0f4523feba2423eb6623df01a6b7c39
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-10-02 18:24:49 -0300

    refact: backed up constants folder

--- UCLoan/Constants/EquipmentConstants.cs
+++ UCLoan/Constants/EquipmentConstants.cs
@@ -1,43 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
-
-namespace UCLoan.Constants
+﻿namespace UCLoan.Constants
 {
     public class EquipmentConstants
     {
         public enum EquipmentLoanStatus
         {
-            [Display(Name = "Indisponível")]
-            Unavailable,
-
-            [Display(Name = "Disponível")]
             Available,
-
-            [Display(Name = "Emprestado")]
-            Borrowed,
-
-            [Display(Name = "Em atraso")]
-            Overdue,
-
-            [Display(Name = "Devolvido")]
-            Returned,
+            Loaned,
+            Reserved,
+            Maintenance
         }
 
         public enum EquipmentPhysicalStatus
         {
-            [Display(Name = "Excelente")]
-            Excellent,
-
-            [Display(Name = "Bom")]
+            New,
             Good,
-
-            [Display(Name = "Regular")]
             Fair,
-
-            [Display(Name = "Ruim")]
             Poor,
-
-            [Display(Name = "Quebrado")]
-            Broken,
+            Broken
         }
     }
 }
