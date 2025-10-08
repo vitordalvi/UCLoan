374445696e58a91f9c8c3450a478815bc958c111
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-10-08 15:48:21 -0300

    feat: new extensions folder to display enums names

---
+++ UCLoan/Extensions/EnumExtensions.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Reflection;
+
+namespace UCLoan.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string GetDisplayName(this Enum enumValue)
+        {
+            var memberInfo = enumValue.GetType().GetMember(enumValue.ToString()).FirstOrDefault();
+
+            if (memberInfo != null)
+            {
+                var displayAttr = memberInfo.GetCustomAttribute<DisplayAttribute>();
+                if (displayAttr != null)
+                    return displayAttr.GetName()!;
+            }
+
+            return enumValue.ToString();
+        }
+    }
+}
