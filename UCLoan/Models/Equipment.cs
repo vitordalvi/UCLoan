50034a9e3fa620199793b9e438904e6b899812af
Vitor Dalvi <114376105+vitordalvi@users.noreply.github.com>
2025-10-08 15:47:58 -0300

    fix: equipment model for db

--- UCLoan/Models/Equipment.cs
+++ UCLoan/Models/Equipment.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using UCLoan.Constants;
 
@@ -6,13 +7,22 @@ namespace UCLoan.Models
 
 {
     [Table("Equipment")]
+    [Index(nameof(EquipmentId), IsUnique = true)]
     public class Equipment
     {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "O patrimônio é obrigatório.")]
         public int EquipmentId { get; set; }
         public EquipmentConstants.EquipmentLoanStatus LoanStatus { get; set; }
+        [Required(ErrorMessage = "Selecione o estado físico.")]
         public EquipmentConstants.EquipmentPhysicalStatus PhysicalStatus { get; set; }
+        [StringLength(255)]
         public string Description { get; set; } = string.Empty;
+        [Required]
+        public int EquipmentModelId { get; set; }
         public EquipmentModel EquipmentModel { get; set; } = null!;
     }
 }
