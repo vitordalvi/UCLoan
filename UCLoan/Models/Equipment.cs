e46c67b824112841368eb42353e891888bd9f071
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-11 04:48:00 -0300

    fix: removed required equipment model id to generate equipments

--- UCLoan/Models/Equipment.cs
+++ UCLoan/Models/Equipment.cs
@@ -21,7 +21,6 @@ namespace UCLoan.Models
         public EquipmentConstants.EquipmentPhysicalStatus PhysicalStatus { get; set; }
         [StringLength(255)]
         public string Description { get; set; } = string.Empty;
-        [Required]
         [Range(1, int.MaxValue, ErrorMessage = "Selecione um modelo válido.")]
         public int EquipmentModelId { get; set; }
         public EquipmentModel EquipmentModel { get; set; } = null!;
