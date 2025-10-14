8616ab50506ed8bd0aaafff0f039bc93246ab0d2
Vitor <vitodalvi@gmail.com>
2025-10-14 10:45:21 -0300

    fix: changed datetime to utcnow

--- UCLoan/Models/Loan.cs
+++ UCLoan/Models/Loan.cs
@@ -11,7 +11,7 @@ namespace UCLoan.Models
 
         [Required]
         public ApplicationUser User { get; set; } = null!;
-        public DateTime StartDate { get; set; } = DateTime.Now;
+        public DateTime StartDate { get; set; } = DateTime.UtcNow;
         public DateTime? EndDate { get; set; }
     }
 }
