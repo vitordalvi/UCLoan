ba878b1e9bd1d55afd700e2d35cf00873f93aec3
Vitor <vitodalvi@gmail.com>
2025-10-14 08:40:52 -0300

    feat: adding features to indexviewmodel

--- UCLoan/ViewModels/Admin/IndexViewModel.cs
+++ UCLoan/ViewModels/Admin/IndexViewModel.cs
@@ -1,4 +1,6 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Numerics;
 using UCLoan.Models;
 using UCLoan.Services;
 
@@ -6,22 +8,25 @@ namespace UCLoan.ViewModels.Admin
 {
     public class IndexViewModel
     {
-        // Cards perfil
-        [Display(Name = "Usuário")]
-        string Name = string.Empty;
+        // Cards usuários
+        [Display(Name = "Usuários Cadastrados")]
+        public int TotalUsers { get; set; } = 0;
 
-        // Cards empréstimos
-        [Display(Name = "Total de Empréstimos")]
-        public int TotalLoans { get; set; }
-        [Display(Name = "Empréstimos Atuais")]
-        public int CurrentLoans { get; set; }
-        [Display(Name = "Empréstimos Atrasados")]
-        public int LateLoans { get; set; }
+        //// Cards empréstimos
+        //[Display(Name = "Total de Empréstimos")]
+        //public int TotalLoans { get; set; }
+        //[Display(Name = "Empréstimos Atuais")]
+        //public int CurrentLoans { get; set; }
+        //[Display(Name = "Empréstimos Atrasados")]
+        //public int LateLoans { get; set; }
 
         // Cards equipamentos
         [Display(Name = "Total de Equipamentos")]
-        public int AllEquipments { get; set; }
+        public int AllEquipments { get; set;  } = 0;
+        [Display(Name = "Equipamentos emprestados")]
+        public int LoanedEquipments { get; set; }
         [Display(Name = "Equipamentos em manutenção")]
         public int MaintanceEquipments { get; set; }
+        
     }
 }
