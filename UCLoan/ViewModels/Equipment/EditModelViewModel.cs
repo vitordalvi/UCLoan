83b63d65a764d337a1c0fdba05520e48893e12c9
Vitor <vitodalvi@gmail.com>
2025-10-13 21:11:04 -0300

    feat: added equipment models view model

---
+++ UCLoan/ViewModels/Equipment/EditModelViewModel.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UCLoan.ViewModels.Equipment
+{
+    public class EditModelViewModel
+    {
+        public int Id { get; set; }
+        [Display(Name = "Nome do Modelo")]
+        [StringLength(60, ErrorMessage = "O nome do modelo não pode exceder 60 caracteres.")]
+        public string Name { get; set; } = string.Empty;
+        [Display(Name = "Fabricante")]
+        [StringLength(50, ErrorMessage = "O nome do fabricante não pode exceder 50 caracteres.")]
+        public string Manufacturer { get; set; } = string.Empty;
+    }
+}
