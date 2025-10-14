6f8b78a9f9bb76f21367337e2292e78bba13b154
Vitor <vitodalvi@gmail.com>
2025-10-14 10:45:07 -0300

    feat: add manage loans get method

--- UCLoan/Controllers/LoanController.cs
+++ UCLoan/Controllers/LoanController.cs
@@ -1,12 +1,43 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using UCLoan.Services;
+using UCLoan.ViewModels.Loan;
 
 namespace UCLoan.Controllers
 {
+    [Authorize(Roles = "Admin")]
     public class LoanController : Controller
     {
+        private readonly LoanService _loanService;
+        private readonly AdminService _adminService;
+        private readonly EquipmentService _equipmentService;
+
+        public LoanController(LoanService loanService, AdminService adminService, EquipmentService equipmentService) {
+            _loanService = loanService;
+            _adminService = adminService;
+            _equipmentService = equipmentService;
+        }
+
+        [HttpGet]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public IActionResult ManageLoans()
+        {
+            var loans = _loanService.GetAllLoansAsync();
+
+            return View(loans);
+        }
+
+        private async Task LoadDropdownsAsync()
+        {
+            ViewBag.AllAvailableEquipments = await _equipmentService.GetAllLoanStatusSelectListAsync();
+            ViewBag.AllEquipmentModels = await _equipmentService.GetAllEquipmentModelsSelectListAsync();
+            ViewBag.AllLoanStatus = await _equipmentService.GetAllLoanStatusSelectListAsync();
+            ViewBag.AllPhysicalStatus = await _equipmentService.GetAllPhysicalStatusSelectListAsync();
         }
     }
 }
