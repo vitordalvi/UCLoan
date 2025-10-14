c58d0c13977ad9552aec51730233194a16d90973
Vitor <vitodalvi@gmail.com>
2025-10-14 19:07:28 -0300

    fix: update add loan view model

--- UCLoan/ViewModels/Loan/AddLoanViewModel.cs
+++ UCLoan/ViewModels/Loan/AddLoanViewModel.cs
@@ -1,13 +1,24 @@
-﻿namespace UCLoan.ViewModels.Loan
+﻿using UCLoan.Services;
+using UCLoan.Models;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel.DataAnnotations;
+
+namespace UCLoan.ViewModels.Loan
 {
     public class AddLoanViewModel
     {
         public int Id { get; set; }
+        [Required(ErrorMessage = "O e-mail do recebedor é obrigatório.")]
         public string UserEmail { get; set; } = string.Empty;
+        [Required(ErrorMessage = "O equipamento escolhido é obrigatório")]
         public int EquipmentId { get; set; }
+        public int SelectedEquipmentId { get; set; }
+        public List<SelectListItem> AvailableEquipments { get; set; } = new List<SelectListItem>();
         public DateTime LoanDate { get; set; }
+        [Required(ErrorMessage = "A data de devolução é obrigatória.")]
         public DateTime? ReturnDate { get; set; }
-        public bool IsReturned { get; set; }
+        [StringLength(80, ErrorMessage = "A sua descrição deve conter um máximo de 80 caracteres.")]
+        public string Description { get; set; } = string.Empty;
 
     }
 }
