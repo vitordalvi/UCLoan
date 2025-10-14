7131ff68451e8122cde17ea19f0905891701fffe
Vitor <vitodalvi@gmail.com>
2025-10-14 19:04:50 -0300

    chore: update db

---
+++ UCLoan/Data/Migrations/20251014213416_Update Database.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace UCLoan.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateDatabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Loans",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified),
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Loans",
+                type: "nvarchar(80)",
+                maxLength: 80,
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<int>(
+                name: "EquipmentId",
+                table: "Loans",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Loans_EquipmentId",
+                table: "Loans",
+                column: "EquipmentId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Loans_Equipment_EquipmentId",
+                table: "Loans",
+                column: "EquipmentId",
+                principalTable: "Equipment",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Loans_Equipment_EquipmentId",
+                table: "Loans");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Loans_EquipmentId",
+                table: "Loans");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Loans");
+
+            migrationBuilder.DropColumn(
+                name: "EquipmentId",
+                table: "Loans");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "EndDate",
+                table: "Loans",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+    }
+}
