ea3ba0d775a5eb5091e8d4146491dd019c8e055d
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-14 22:30:37 -0300

    fix: remove loanstatus change option

--- UCLoan/ViewModels/Equipment/EditEquipmentViewModel.cs
+++ UCLoan/ViewModels/Equipment/EditEquipmentViewModel.cs
@@ -13,8 +13,6 @@ namespace UCLoan.ViewModels.Equipment
         public string? Description { get; set; }
         [Display(Name = "Estado Físico")]
         public EquipmentConstants.EquipmentPhysicalStatus PhysicalStatus { get; set; }
-        [Display(Name = "Estado Empréstimo")]
-        public EquipmentConstants.EquipmentLoanStatus LoanStatus { get; set; }
         [Range(1, int.MaxValue, ErrorMessage = "Selecione um modelo válido.")]
         [Display(Name = "Modelo do Equipamento")]
         public int EquipmentModelId { get; set; }
