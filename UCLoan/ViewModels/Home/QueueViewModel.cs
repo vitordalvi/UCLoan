5c32a97ad70cb4a0029298111dd8663a06d41d35
Vitor <vitodalvi@gmail.com>
2025-10-16 18:33:06 -0300

    feat: added queue view model

---
+++ UCLoan/ViewModels/Home/QueueViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace UCLoan.ViewModels.Home
+{
+    public class QueueViewModel
+    {
+        public string UserEmail { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime EndDate { get; set; } = DateTime.UtcNow;
+    }
+}
