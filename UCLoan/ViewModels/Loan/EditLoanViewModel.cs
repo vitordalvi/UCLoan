809b69cfbcf0daaf4f3aec2e519b62ea075fe242
Vitor <vitodalvi@gmail.com>
2025-10-15 22:53:59 -0300

    feat: add edit loan viewmodel

--- UCLoan/ViewModels/Loan/EditLoanViewModel.cs
+++ UCLoan/ViewModels/Loan/EditLoanViewModel.cs
@@ -1,6 +1,16 @@
-﻿namespace UCLoan.ViewModels.Loan
+﻿using System.ComponentModel.DataAnnotations;
+using UCLoan.Constants;
+
+namespace UCLoan.ViewModels.Loan
 {
     public class EditLoanViewModel
     {
+        public int Id { get; set; }
+        public int EquipmentId { get; set; }
+        public EquipmentConstants.EquipmentLoanStatus LoanStatus { get; set; }
+        public EquipmentConstants.EquipmentPhysicalStatus PhysicalStatus { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime? EndDate { get; set; }
+        public string Description { get; set; } = string.Empty;
     }
 }
