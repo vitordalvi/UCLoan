a70710cfb9466e82ba65ae9385e1c7cf354188bd
Vitor <vitodalvi@gmail.com>
2025-10-15 22:56:19 -0300

    fix: allowed username characters

--- UCLoan/Program.cs
+++ UCLoan/Program.cs
@@ -45,7 +45,7 @@ builder.Services.Configure<IdentityOptions>(options =>
 
     // Configuracoes de usuario
     options.User.AllowedUserNameCharacters =
-        "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
+        "„√abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789-._@+";
     options.User.RequireUniqueEmail = true;
 
     // Configuracoes de login
