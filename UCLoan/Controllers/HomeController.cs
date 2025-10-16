78472cd76491b9ba836b6fde3bfeadbbfa1c3f07
Vitor <vitodalvi@gmail.com>
2025-10-16 05:26:08 -0300

    feat: index homepage for users working

--- UCLoan/Controllers/HomeController.cs
+++ UCLoan/Controllers/HomeController.cs
@@ -1,9 +1,14 @@
 using System.Diagnostics;
+using System.Security.Claims;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using NuGet.Versioning;
+using UCLoan.Extensions;
 using UCLoan.Models;
+using UCLoan.Services;
+using UCLoan.ViewModels.Home;
 
 namespace UCLoan.Controllers
 {
@@ -11,15 +16,57 @@ namespace UCLoan.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly AdminService _adminService;
+        private readonly EquipmentService _equipmentService;
+        private readonly LoanService _loanService;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(ILogger<HomeController> logger, AdminService adminService, EquipmentService equipmentService, LoanService loanService)
         {
             _logger = logger;
+            _adminService = adminService;
+            _equipmentService = equipmentService;
+            _loanService = loanService;
         }
 
-        public IActionResult Index()
+        [HttpGet]
+        public async Task<IActionResult> Index()
         {
-            return View();
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            
+            if (string.IsNullOrEmpty(userId))
+            {
+                return Challenge();
+            }
+
+            var user = await _adminService.GetByIdAsync(userId);
+
+            if (user == null)
+            {
+                return Forbid();
+            }
+
+            var userRole = await _adminService.GetUserRolesAsync(user);
+            var userLoans = await _loanService.GetUserLoansByEmailAsync(user.Email!);
+            var userEquipments = await _loanService.GetUserLoansEquipmentByEmailAsync(user.Email!);
+
+            var viewModel = new IndexViewModel
+            {
+                UserName = user.FullName,
+                UserEmail = user.Email!,
+                PhoneNumber = user.PhoneNumber!,
+                CPF = user.CPF,
+                Role = userRole.FirstOrDefault()!,
+                ActiveLoans = userLoans.Count,
+                LoanId = userLoans.FirstOrDefault()?.Id ?? 0,
+                LoanStartDate = userLoans.FirstOrDefault()?.StartDate,
+                LoanEndDate = userLoans.FirstOrDefault()?.EndDate,
+                EquipmentManufacturer = userEquipments.FirstOrDefault()?.EquipmentModel?.Manufacturer ?? "Nenhum equipamento emprestado",
+                EquipmentName = userEquipments.FirstOrDefault()?.EquipmentModel?.Name ?? "Nenhum equipamento emprestado",
+                EquipmentId = userEquipments.FirstOrDefault()?.EquipmentId ?? 0,
+                EquipmentPhysicalStatus = userEquipments.FirstOrDefault()?.PhysicalStatus.GetDisplayName() ?? "Nenhum equipamento emprestado"
+            };
+
+            return View(viewModel);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
