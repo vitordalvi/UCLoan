bf682170e82d51495ff59832cb98fe26ef9af22f
Vitor <vitodalvi@gmail.com>
2025-10-15 22:49:37 -0300

    chore: update db

--- UCLoan/Data/Migrations/20251015192911_Update LoanStatus in LoanModel.Designer.cs
+++ UCLoan/Data/Migrations/20251015192911_Update LoanStatus in LoanModel.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using UCLoan.Data;
 
@@ -11,9 +12,11 @@ using UCLoan.Data;
 namespace UCLoan.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251015192911_Update LoanStatus in LoanModel")]
+    partial class UpdateLoanStatusinLoanModel
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
