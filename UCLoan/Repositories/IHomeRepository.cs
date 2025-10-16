15b0d6983dd84d8d7a453e61bcfe804552b0910b
Vitor <vitodalvi@gmail.com>
2025-10-16 18:31:52 -0300

    feat: home repository + interface with db query

---
+++ UCLoan/Repositories/IHomeRepository.cs
@@ -0,0 +1,20 @@
+﻿namespace UCLoan.Repositories
+{
+    public interface IHomeRepository
+    {
+        Task<List<Models.Queue>> GetAllQueuesAsync(CancellationToken ct = default);
+        Task<Models.Queue?> GetQueueByIdAsync(int id, CancellationToken ct = default);
+        Task<int> GetTotalQueuesAsync(CancellationToken ct = default);
+        Task<Models.Queue?> GetQueueByUserIdAsync(string userId, CancellationToken ct = default);
+        Task<Models.Queue?> GetQueueByUserEmailAsync(string email, CancellationToken ct = default);
+        Task<bool> RemoveQueueAsync(int id, CancellationToken ct = default);
+        Task<bool> RemoveQueueByUserIdAsync(string userId, CancellationToken ct = default);
+        Task<List<Models.Queue>> GetQueuesOrderedByCreationAsync(CancellationToken ct = default);
+        Task AddQueueAsync(Models.Queue queue, CancellationToken ct = default);
+        Task<int> GetQueuePositionByUserIdAsync(string userId, CancellationToken ct = default);
+        Task<Models.Queue?> GetLatestQueueAsync(CancellationToken ct = default);
+        Task<bool> IsUserInQueueAsync (string userId, CancellationToken ct = default);
+        Task<int> GetUserPositionInQueueAsync(string userId, CancellationToken ct = default);
+        Task<bool> SaveChangesAsync(CancellationToken ct = default);
+    }
+}
