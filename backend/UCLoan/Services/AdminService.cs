d826f56552e573d8ac64f159c765b1bebd8007ac
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 14:20:21 -0300

    feat: add admin basic services

---
+++ backend/UCLoan/Services/AdminService.cs
@@ -0,0 +1,21 @@
+﻿using UCLoan.Entities;
+using UCLoan.Repository;
+
+namespace UCLoan.Services
+{
+    public class AdminService : BaseService
+    {
+        private readonly IUserRepository _userRepository;
+        public AdminService(IUserRepository userRepository, LogService logService) : base(logService)
+        {
+            _userRepository = userRepository;
+        }
+
+        // Obtém um usuário pelo seu ID
+        public Task<User?> GetByIdAsync(Guid userId) =>
+            _userRepository.GetByIdAsync(userId);
+        // Obtem um usuário pelo seu email
+        public Task<User?> GetByEmailAsync(string email) =>
+            _userRepository.GetByEmailAsync(email);
+    }
+}
