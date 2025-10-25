38ab5d82fae245ade40a7446138ed2acb7613b84
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:29:30 -0300

    feat: created enums related to queue

---
+++ backend/UCLoan/Constants/Queue.cs
@@ -0,0 +1,16 @@
+﻿namespace UCLoan.Constants
+{
+    public enum QueueStatus
+    {
+        Cancelled = 0,
+        Pending = 1,
+        Served = 2,
+    }
+
+    public enum QueuePriority
+    {
+        Low = 0,
+        Normal = 1,
+        High = 2,
+    }
+}
