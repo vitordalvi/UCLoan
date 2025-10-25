a8b6ac6d26034dbc82a6a1b9d471c47036a4c724
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:54:43 -0300

    feat: add expires at property in TokenReponseDTO

--- backend/UCLoan/DTOs/Authentication/TokenResponseDTO.cs
+++ backend/UCLoan/DTOs/Authentication/TokenResponseDTO.cs
@@ -4,5 +4,6 @@
     {
         public required string AccessToken { get; set; }
         public required string RefreshToken { get; set; }
+        public DateTime ExpiresAt { get; set; } = DateTime.UtcNow;
     }
 }
