cea342554c0eab7e87626931ec1294974d33797f
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:58:37 -0300

    feat: new activity log service feature

---
+++ backend/UCLoan/Services/LogService.cs
@@ -0,0 +1,31 @@
+﻿using System.Text.Json;
+using UCLoan.Entities;
+using UCLoan.Repository;
+
+namespace UCLoan.Services
+{
+    public class LogService
+    {
+        private readonly ILogRepository _logRepository;
+        public LogService(ILogRepository logRepository)
+        {
+            _logRepository = logRepository;
+        }
+
+        // Cria o registro da atividade (*Posso criar uma validação aqui depois, se for necessário realmente*)
+        public async Task LogAsync(Guid userId, string action, string entityName = "", Guid? entityId = null, object? details = null)
+        {
+            var log = new ActivityLog
+            {
+                UserId = userId,
+                Action = action,
+                EntityName = entityName,
+                EntityId = entityId,
+                DetailsJson = details != null ? JsonSerializer.Serialize(details) : string.Empty,
+                OccurredAt = DateTime.UtcNow
+            };
+
+            await _logRepository.AddLogAsync(log);
+        }
+    }
+}
