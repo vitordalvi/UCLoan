cec40fc6862e56dbbe19745eb12639eebc9fbbdf
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:56:23 -0300

    feat: new repository tasks + docs

--- backend/UCLoan/Repository/AuthRepository.cs
+++ backend/UCLoan/Repository/AuthRepository.cs
@@ -12,17 +12,35 @@ namespace UCLoan.Repository
             _context = context;
         }
 
+        // Adiciona um novo usuário no banco de dados
         public async Task AddUserAsync(User user)
         {
             await _context.Set<User>().AddAsync(user);
         }
 
-        public async Task<User> GetUserByEmailAsync(string email)
+        // Obtém um usuário pelo seu ID
+        public async Task<User?> GetUserByIdAsync(Guid userId)
+        {
+            return await _context.Users
+                .FindAsync(userId);
+        }
+
+        // Obtém um usuário pelo seu email
+        public async Task<User?> GetUserByEmailAsync(string email)
         {
             return await _context.Users
                 .FirstOrDefaultAsync(u => u.Email == email);
         }
 
+        // Obtem um usuário pelo seu CPF
+        public async Task<User?> GetUserByCpfAsync(string CPF)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(c => c.CPF == CPF);
+        }
+
+        // Salva as mudanças no banco de dados
+        // Se > 0, retorna True, entrando pra validação de sucesso
         public async Task<bool> SaveChangesAsync() =>
             await _context.SaveChangesAsync() > 0;
     }
--- backend/UCLoan/Repository/IAuthRepository.cs
+++ backend/UCLoan/Repository/IAuthRepository.cs
@@ -4,8 +4,15 @@ namespace UCLoan.Repository
 {
     public interface IAuthRepository
     {
+        // Adicionar um novo usuário
         Task AddUserAsync(User user);
-        Task<User> GetUserByEmailAsync(string email);
+        // Obter usuário por Id
+        Task<User?> GetUserByIdAsync(Guid userId);
+        // Obter usuário por email
+        Task<User?> GetUserByEmailAsync(string email);
+        // Obter usuário por CPF
+        Task<User?> GetUserByCpfAsync(string CPF);
+        // Salvar mudanças no banco de dados
         Task<bool> SaveChangesAsync();
     }
 }
