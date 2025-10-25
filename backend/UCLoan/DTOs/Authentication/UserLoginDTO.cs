450b9ac97cd7f1f47595e335703e58bf50d13333
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:55:08 -0300

    feat: created a DTO to user while logging

---
+++ backend/UCLoan/DTOs/Authentication/UserLoginDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UCLoan.DTOs.Authentication
+{
+    public class UserLoginDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
