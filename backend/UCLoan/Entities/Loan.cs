25b734d4b5908145a3f2222890f97459f316f1cf
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:31:01 -0300

    feat: add loan entity

---
+++ backend/UCLoan/Entities/Loan.cs
@@ -0,0 +1,17 @@
+﻿using UCLoan.Constants;
+
+namespace UCLoan.Entities
+{
+    public class Loan
+    {
+        public Guid Id { get; set; }
+        public Guid EquipmentId { get; set; }
+        public LoanStatus Status { get; set; }
+        public Equipment? Equipment { get; set; }
+        public Guid UserId { get; set; }
+        public User? User { get; set; }
+        public DateTime LoanDate { get; set; }
+        public DateTime? ReturnDate { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
