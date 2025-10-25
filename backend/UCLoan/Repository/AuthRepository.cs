156bc17be5f9967dbdeaa9f6d9e35d337affdbad
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:33:25 -0300

    feat: added initial repository features

---
+++ backend/UCLoan/Repository/AuthRepository.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UCLoan.Data;
+using UCLoan.Entities;
+
+namespace UCLoan.Repository
+{
+    public class AuthRepository : IAuthRepository
+    {
+        private readonly AppDbContext _context;
+        public AuthRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task AddUserAsync(User user)
+        {
+            await _context.Set<User>().AddAsync(user);
+        }
+
+        public async Task<User> GetUserByEmailAsync(string email)
+        {
+            return await _context.Users
+                .FirstOrDefaultAsync(u => u.Email == email);
+        }
+    }
+}
---
+++ backend/UCLoan/Repository/IAuthRepository.cs
@@ -0,0 +1,10 @@
+﻿using UCLoan.Entities;
+
+namespace UCLoan.Repository
+{
+    public interface IAuthRepository
+    {
+        Task AddUserAsync(User user);
+        Task<User> GetUserByEmailAsync(string email);
+    }
+}
