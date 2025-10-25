88965696777edfb46d597b0f78379f52c9c82a07
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:55:23 -0300

    feat: created user register DTO while registering

---
+++ backend/UCLoan/DTOs/Authentication/UserRegisterDTO.cs
@@ -0,0 +1,12 @@
+﻿using UCLoan.Constants;
+
+namespace UCLoan.DTOs.Authentication
+{
+    public class UserRegisterDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
