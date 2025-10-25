c7e86f5ca557b1b6c0b41e4f5cbe663554026009
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:56:57 -0300

    feat: new activity log feature repository + interfaces

---
+++ backend/UCLoan/Repository/ILogRepository.cs
@@ -0,0 +1,12 @@
+﻿using UCLoan.Entities;
+
+namespace UCLoan.Repository
+{
+    public interface ILogRepository
+    {
+        // Interface para adicionar uma log no banco de dados
+        Task AddLogAsync(ActivityLog log);
+        // Interface para obter as logs de um usuário específico
+        Task<List<ActivityLog>> GetUserLogsAsync(Guid userId);
+    }
+}
---
+++ backend/UCLoan/Repository/LogRepository.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UCLoan.Data;
+using UCLoan.Entities;
+
+namespace UCLoan.Repository
+{
+    public class LogRepository : ILogRepository
+    {
+        private readonly AppDbContext _context;
+        public LogRepository(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // Adiciona uma log no banco de dados
+        public async Task AddLogAsync(ActivityLog log)
+        {
+            await _context.ActivityLogs.AddAsync(log);
+            await _context.SaveChangesAsync();
+        }
+
+        // Obtém as logs de um usuário específico
+        public async Task<List<ActivityLog>> GetUserLogsAsync(Guid userId)
+        {
+            return await _context.ActivityLogs
+                .Where(a => a.UserId == userId)
+                .OrderByDescending(a => a.OccurredAt)
+                .ToListAsync();
+        }
+    }
+}
