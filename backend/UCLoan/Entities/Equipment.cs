d278509e0522d5f01f56cc5968d03bd176b5a913
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:30:53 -0300

    feat: add equipment entity

---
+++ backend/UCLoan/Entities/Equipment.cs
@@ -0,0 +1,14 @@
+﻿using UCLoan.Constants;
+
+namespace UCLoan.Entities
+{
+    public class Equipment
+    {
+        public Guid Id { get; set; }
+        public PhysicalStatus PhysicalStatus { get; set; }
+        public LoanStatus LoanStatus { get; set; }
+        public Guid ModelId { get; set; }
+        public string Description { get; set; } = string.Empty;
+        public Model? Model { get; set; }
+    }
+}
