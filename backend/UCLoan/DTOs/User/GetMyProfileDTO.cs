0fdcd8101e401500779c449a4c5bff74c86b0aae
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 14:17:32 -0300

    feat: created user DTOs

---
+++ backend/UCLoan/DTOs/User/GetMyProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace UCLoan.DTOs.User
+{
+    public class GetMyProfileDTO
+    {
+        public Guid Id { get; set; }
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string CPF { get; set; } = string.Empty;
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ backend/UCLoan/DTOs/User/UpdateMyDataDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace UCLoan.DTOs.User
+{
+    public class UpdateMyDataDTO
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+    }
+}
