c5acb05970a2f7d3e1c629aa745ea50825b39b0b
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:58:21 -0300

    feat: new baseservice feature to improve the usage of the  activity log

---
+++ backend/UCLoan/Services/BaseService.cs
@@ -0,0 +1,24 @@
+﻿using UCLoan.Data;
+
+namespace UCLoan.Services
+{
+    public abstract class BaseService
+    {
+        protected readonly LogService _logService;
+        protected BaseService(LogService logService)
+        {
+            _logService = logService;
+        }
+
+        // Implementa a criação da log, a partir dos serviços que herdam essa classe base
+        protected async Task LogActionAsync(
+            Guid userId,
+            string action,
+            string entityName = "",
+            Guid? entityId = null,
+            object? details = null)
+        {
+            await _logService.LogAsync(userId, action, entityName, entityId, details);
+        }
+    }
+}
