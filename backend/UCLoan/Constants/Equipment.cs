9410d78aa78075e3658c13c551b1de65a97cb567
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:29:23 -0300

    feat: created enums related to equipment

---
+++ backend/UCLoan/Constants/Equipment.cs
@@ -0,0 +1,20 @@
+﻿namespace UCLoan.Constants
+{
+    public enum PhysicalStatus
+    {
+        Excellent = 0,
+        Good = 1,
+        Fair = 2,
+        Poor = 3,
+        Damaged = 4,
+    }
+
+    public enum LoanStatus
+    {
+        Unavailable = 0,
+        Available = 1,
+        Borrowed = 2,
+        Overdue = 3,
+        Returned = 4,
+    }
+}
