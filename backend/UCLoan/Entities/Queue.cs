f3a1b1167636bd5f7d804612b61d7bd74f2fc463
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:31:16 -0300

    feat: add queue entity

---
+++ backend/UCLoan/Entities/Queue.cs
@@ -0,0 +1,14 @@
+﻿using UCLoan.Constants;
+
+namespace UCLoan.Entities
+{
+    public class Queue
+    {
+        public Guid Id { get; set; }
+        public string UserId { get; set; } = string.Empty;
+        public int Position { get; set; }
+        public QueuePriority Priority { get; set; } = QueuePriority.Normal;
+        public QueueStatus Status { get; set; } = QueueStatus.Pending;
+        public DateTime EnqueuedAt { get; set; } = DateTime.UtcNow;
+    }
+}
