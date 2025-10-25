0c0c51568e8ef0748673a9fc5c338e4c07057367
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 10:54:18 -0300

    fix: removed unique userdto

--- backend/UCLoan/DTOs/Authentication/UserDTO.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace UCLoan.DTOs.Authentication
-{
-    public class UserDTO
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
