6cc67d56cd39fe45f4df44c5e9d85832db42dbf1
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 14:16:43 -0300

    feat: added user controller with getmyprofile endpoint + updatemydata endpoint

---
+++ backend/UCLoan/Controllers/UserController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+using UCLoan.DTOs.User;
+using UCLoan.Services;
+
+namespace UCLoan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class UserController : ControllerBase
+    {
+        private readonly UserService _userService;
+        public UserController(UserService userService)
+        {
+            _userService = userService;
+        }
+
+        [HttpGet("me")]
+        public async Task<IActionResult> GetMyProfile()
+        {
+            var (success, message, user) = await _userService.GetMyProfileAsync();
+
+            if (!User.Identity!.IsAuthenticated)
+                return Unauthorized("Usuário não autenticado.");
+
+            if (!success)
+                return BadRequest(message);
+
+            return Ok(new { success, message, user });
+        }
+
+        [HttpPost("update-my-data")]
+        public async Task<IActionResult> UpdateMyData(UpdateMyDataDTO dto)
+        {
+            var (success, message) = await _userService.UpdateMyData(dto);
+
+            if (!success)
+                return BadRequest(message);
+
+            return Ok(new { success, message });
+        }
+    }
+}
