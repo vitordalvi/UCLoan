95a36f90b9c947abce6d28d5c577cf4abb97ef0a
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:31:08 -0300

    feat: add equipment model entity

---
+++ backend/UCLoan/Entities/Model.cs
@@ -0,0 +1,10 @@
+﻿namespace UCLoan.Entities
+{
+    public class Model
+    {
+        public Guid? Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Manufacturer { get; set; } = string.Empty;
+        public virtual ICollection<Equipment> Equipments { get; set; } = new List<Equipment>();
+    }
+}
