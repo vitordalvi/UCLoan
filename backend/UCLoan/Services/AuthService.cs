df5f4b90cca2c1184ed9bd2a99d07902e29430b2
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-25 04:33:44 -0300

    feat: started auth services

---
+++ backend/UCLoan/Services/AuthService.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.Reflection.Metadata.Ecma335;
+using UCLoan.DTOs.Authentication;
+using UCLoan.Entities;
+using UCLoan.Repository;
+
+namespace UCLoan.Services
+{
+    public class AuthService
+    {
+        private readonly IAuthRepository _authRepository;
+        public AuthService(IAuthRepository authRepository)
+        {
+            _authRepository = authRepository;
+        }
+
+        // Obter usuário por email
+        public Task<User> GetUserByEmailAsync(string email) =>
+            _authRepository.GetUserByEmailAsync(email);
+
+        //public async Task<(bool Success, string Error)> RegisterAsync(UserDTO request)
+        //{
+        //    if (await _authRepository.GetUserByEmailAsync(request.Email) != null)
+        //    {
+        //        return (false, "O usuário já existe.");
+        //    }
+
+
+            
+        //}
+    }
+}
