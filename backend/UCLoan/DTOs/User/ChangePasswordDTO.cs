b79284cb170f4d39953db572a46653021404c950
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-27 14:56:39 -0300

    feat: new dto for users to change their password

---
+++ backend/UCLoan/DTOs/User/ChangePasswordDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace UCLoan.DTOs.User
+{
+    public class ChangePasswordDTO
+    {
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+        public string ConfirmNewPassword { get; set; } = string.Empty;
+    }
+}
