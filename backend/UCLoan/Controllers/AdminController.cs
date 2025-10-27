d3a33e1a71269b53de713c41139308f7141729a3
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-27 13:38:23 -0300

    feat: new get-all-users endpoint

---
+++ backend/UCLoan/Controllers/AdminController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UCLoan.Constants;
+using UCLoan.Entities;
+using UCLoan.Services;
+
+namespace UCLoan.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize(Roles = "Admin")]
+    public class AdminController : ControllerBase
+    {
+        private readonly AdminService _adminService;
+        public AdminController(AdminService adminService)
+        {
+            _adminService = adminService;
+        }
+
+        [HttpGet("get-all-users")]
+        public async Task<ActionResult<IList<User>>> GetUsersAsync()
+        {
+            var users = await _adminService.GetUsersAsync();
+
+            return Ok(users);
+        }
+    }
+}
