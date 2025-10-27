cf4ab1646ea94f4465739cd4534c9400791cf74c
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-27 13:36:19 -0300

    feat: added Role property

--- backend/UCLoan/DTOs/User/GetMyProfileDTO.cs
+++ backend/UCLoan/DTOs/User/GetMyProfileDTO.cs
@@ -6,6 +6,7 @@
         public string Email { get; set; } = string.Empty;
         public string Name { get; set; } = string.Empty;
         public string CPF { get; set; } = string.Empty;
+        public string Role { get; set; } = string.Empty;
         public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     }
 }
