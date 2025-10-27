c5fff619bc19165a22528c08d22cf88822fc1921
Vitor Dalvi <vitodalvi@gmail.com>
2025-10-27 13:39:12 -0300

    docs: added interface documentation

--- backend/UCLoan/Repository/IUserRepository.cs
+++ backend/UCLoan/Repository/IUserRepository.cs
@@ -4,11 +4,21 @@ namespace UCLoan.Repository
 {
     public interface IUserRepository
     {
+        // Método para pegar o ID do usuário logado a partir do token JWT
+        Task<Guid> GetCurrentUserId();
+        // Obtém um usuário pelo seu ID
         Task<User?> GetByIdAsync(Guid userId);
+        // Obtém um usuário pelo seu email
         Task<User?> GetByEmailAsync(string email);
+        // Obtém um usuário pelo seu CPF
         Task<User?> GetByCpfAsync(string CPF);
-        Task UpdateAsync(User user);
+        // Verifica se o e-mail já está em uso
         Task<bool> IsEmailInUse(string email);
+        // Lista todos os usuários
+        Task<IList<User>> GetUsersAsync();
+        // Atualiza os dados do usuário
+        Task UpdateAsync(User user);
+        // Salva os dados no banco
         Task<bool> SaveChangesAsync();
     }
 }
